3c190e2920829b55aba8e981d2f3e1dad15288cd
hendri27 <88545100+hendris27@users.noreply.github.com>
2023-10-12 12:20:20 +0700

    first commit

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import Home from './src/screens/Home';
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
